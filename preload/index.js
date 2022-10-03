cf4e97bd7bd6bb37f5e8afbefc5ca982dc8f324c
lexur <2259334682@qq.com>
2022-10-03 09:05:10 +0800

    修复为分p视频错误

--- preload/index.js
+++ preload/index.js
@@ -3,7 +3,13 @@ const bv2Notion = (url) => {
     // const re = /(BV.*?).{10}/
     // const result = re.exec(url);
     const bv = url.match(/(BV.*?).{10}/)[0]
-    const p = url.match(url.match(/p=(\d+)/)[1])
+    let p = null;
+    try {
+        p = url.match(url.match(/p=(\d+)/)[1]);
+    } catch (error) {
+        p = null
+    }
+    
     let result = "player.bilibili.com/player.html?bvid="+bv
     if (p) {
         result += "&p="+p
